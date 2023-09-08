851a4c825d083f8774f12558f37a910767eaa8a8
code-yesiam <132221618+code-yesiam@users.noreply.github.com>
2023-09-08 19:34:08 -0300

    Make the Details Info for houses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {RouterModule} from '@angular/router'
+
 import {HomeComponent} from './home/home.component'
 
 @Component({
@@ -7,14 +9,17 @@ import {HomeComponent} from './home/home.component'
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
   <main>
+    <a [routerLink]="['/']">
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
   `,
